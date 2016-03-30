8190aedca512d727a90f62088e97dc15df469c1f
Guro Bokum <jiojiajiu@gmail.com>
2016-03-30 12:39:09 +0300

    Add mongo collection Tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,25 @@
 import {Template} from 'meteor/templating';
 
+import {Tasks} from '../api/tasks.js';
+
 import './body.html';
 
 Template.body.helpers({
-    tasks: [{
-        text: 'This is task 1',
-    }, {
-        text: 'This is task 2',
-    }, {
-        text: 'This is task 3',
-    }]
-})
+    tasks: Tasks.find({}, {sort: {createdAt: -1}}
+});
+
+Template.body.events({
+    'submit .new-task'(event) {
+        event.preventDefault();
+
+        var target = event.target;
+        var text = target.text.value;
+
+        Tasks.insert({
+            text: text,
+            createdAt: Date.now(),
+        })
+
+        target.text.value = '';
+    }
+});
