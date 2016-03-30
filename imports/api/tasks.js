377b0a7a91f4ce5d4a63962e28434eb4f7f9ae80
Guro Bokum <jiojiajiu@gmail.com>
2016-03-30 15:23:34 +0300

    Add pubsub and additional methods checks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,18 @@ import {check} from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+    Meteor.publish('tasks', function () {
+        return Tasks.find({
+            $or: [{
+                private: {$ne: true}
+            }, {
+                owner: this.userId
+            }]
+        });
+    })
+}
+
 Meteor.methods({
     'tasks.insert'(text) {
         check(text, String);
@@ -20,9 +32,15 @@ Meteor.methods({
         })
     },
 
-    'tasks.update'(id, checked) {
+    'tasks.setChecked'(id, checked) {
         check(id, String);
         check(checked, Boolean);
+
+        const task = Task.findOne(id);
+
+        if (taks.private && task.owner !== Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
 
         Tasks.update(id, {
             $set: {
@@ -31,8 +49,31 @@ Meteor.methods({
         })
     },
 
+    'tasks.setPrivate'(id, private) {
+        check(id, String);
+        check(private, Boolean);
+
+        const task = Tasks.findOne(id);
+
+        if (task.owner !== Meteor.userId()) {
+            throw new Meteor.Error('not-autorized');
+        }
+
+        Tasks.update(id, {
+            $set: {
+                private: private
+            }
+        })
+    },
+
     'tasks.remove'(id) {
         check(id, String);
+
+        const task = Tasks.findOne(id);
+
+        if (task.private && task.owner !== Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
 
         Tasks.remove(id);
     }
--- imports/ui/task.js
+++ imports/ui/task.js
@@ -5,12 +5,22 @@ import {Tasks} from '../api/tasks.js';
 
 import './task.html';
 
+Template.task.helpers({
+    isOwner() {
+        return this.owner === Meteor.userId()
+    }
+});
+
 Template.task.events({
     'click .toggle-checked'(event) {
-        Meteor.call('tasks.update', this._id, !this.checked);
+        Meteor.call('tasks.setChecked', this._id, !this.checked);
+    },
+
+    'click .toggle-private'(event) {
+        Meteor.call('tasks.setPrivate', this._id, !this.private);
     },
 
     'click .delete'(event) {
         Meteor.call('tasks.remove', this._id);
-    }
+    },
 });
