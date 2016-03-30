1c4fa7d6e91d53acaca0d9ef0f3d4ee79e144795
Guro Bokum <jiojiajiu@gmail.com>
2016-03-30 14:16:10 +0300

    Add filtering

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,35 @@
 import {Template} from 'meteor/templating';
+import {ReactiveDict} from 'meteor/reactive-dict';
 
 import {Tasks} from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function () {
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
-    tasks: Tasks.find({}, {sort: {createdAt: -1}})
+    tasks() {
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+            return Tasks.find({
+                checked: {
+                    $ne: true
+                }
+            }, {sort: {createdAt: -1}})
+        }
+        return Tasks.find({}, {sort: {createdAt: -1}})
+    },
+
+    incompleteCount() {
+        return Tasks.find({
+            checked: {
+                $ne: true
+            }
+        }).count()
+    }
 });
 
 Template.body.events({
@@ -22,5 +45,9 @@ Template.body.events({
         })
 
         target.text.value = '';
+    },
+
+    'click .hide-completed'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
     }
 });
